7396f39f0a7d411a31e479fc663ee5227af14326
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-14 22:50:43 +0600

    Make Firebase initialization failure visible in debug

--- lib/main_clean.dart
+++ lib/main_clean.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
@@ -7,8 +8,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp();
-  } catch (_) {
-    // Firebase platform files can be added later. The app must still run without push setup.
+  } catch (error) {
+    if (kDebugMode) {
+      debugPrint('Firebase initialization skipped: $error');
+    }
   }
   runApp(const MobileChatApp());
 }
