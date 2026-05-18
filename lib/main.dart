a2376be700c1e786e5d982de8c84c857baf74513
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-18 15:19:39 +0600

    Use single Firebase-aware Flutter entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+  } catch (error) {
+    if (kDebugMode) {
+      debugPrint('Firebase initialization skipped: $error');
+    }
+  }
   runApp(const MobileChatApp());
 }
